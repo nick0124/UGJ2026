a278e1477a654e93bc0815c8ba793acc28648ab8
Никита <nick0124creator@gmail.com>
2026-03-22 22:55:22 +0500

    Add turret targeting and bullet damage

--- Assets/Spawner/Scripts/Turret/Turret.cs
+++ Assets/Spawner/Scripts/Turret/Turret.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
 
-public class Turret : MonoBehaviour
+public class Turret : MonoBehaviour, IDamageble
 {
     [field: SerializeField] public Texture Icon { get; private set; }
     [field: SerializeField] public int ID { get; private set; }
+    [field: SerializeField] public float Health { get; private set; }
+
+    public void Die()
+    {
+        Debug.Log($"{gameObject.name} turret is destroyed.");
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Health -= damage;
+
+        if (Health <= 0)
+        {
+            Die();
+        };
+    }
 }
