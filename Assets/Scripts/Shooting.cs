6d8e687f6af3127368b4766f6ca21e8c8cc3174e
Никита <nick0124creator@gmail.com>
2026-03-28 03:28:34 +0500

    Add bullets collor and glow

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,10 +6,21 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab;  // Префаб пули
     public Transform firePoint;       // Точка выстрела
     public float bulletSpeed = 20f;   // Скорость пули
+
+    [Header("Настройки размера")]
+    public float bulletScale = 1f; 
+
+    [Header("Настройки материала")]
+    public Material bulletMaterial;
        
     public void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        Renderer renderer = bullet.GetComponent<Renderer>();
+        renderer.material = new Material(bulletMaterial);
+
+        bullet.transform.localScale *= bulletScale;
         
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         if (rb != null)
